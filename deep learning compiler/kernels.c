84a3168c308d983127a239c4cf9ddba3044dffc1
srihari <sriharimarch@gmail.com>
2026-08-06 23:58:01 +0530

    Implemented add() and relu() with SIMD with ARM NEON.

--- deep learning compiler/kernels.c
+++ deep learning compiler/kernels.c
@@ -1,10 +1,19 @@
 #include <stddef.h>
 #include <string.h>
 #include <math.h>
+#include <arm_neon.h>
 
 /* ELEMENT-WISE OPERATIONS */
 void add(const float* a, const float* b, float* out, int n){
-    for(int i = 0; i < n; i++){
+    int i = 0;
+
+    for (; i + 4 <= n; i +=4 ){
+        float32x4_t va = vld1q_f32(a + i);
+        float32x4_t vb = vld1q_f32(b + i);
+        vst1q_f32(out + i, vaddq_f32(va, vb));
+    }
+
+    for(; i < n; i++){
         out[i] = a[i] + b[i];
     }
 }
@@ -22,7 +31,14 @@ void mul(const float* a, const float* b, float* out, int n){
 }
 
 void relu(const float* x, float* out, int n){
-    for (int i = 0; i < n; i++){
+    float32x4_t zero = vdupq_n_f32(0.0f);
+    int i = 0;
+
+    for (; i + 4 <= n; i += 4){
+        float32x4_t v = vld1q_f32(x + i);
+        vst1q_f32(out + i, vmaxq_f32(v, zero));
+    }
+    for (; i < n; i++){
         out[i] = x[i] > 0.0f ? x[i] : 0.0f;
     }
 }
@@ -36,7 +52,6 @@ void exp_kernel(const float* x, float* out, int n){
 // 64x64 floats = 16 KB 
 #define BLOCK 64
 
-// mkn = mnp
 void matmul(const float* a, const float* b, float* out, int m, int n, int p){
 
     /* out is (m x p), so zero m*p floats. */
@@ -64,4 +79,3 @@ void matmul(const float* a, const float* b, float* out, int m, int n, int p){
         }
     }
 }
-
